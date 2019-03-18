acd54f9a84e7081bcec9f3697f9b6165880f93a1
Doctor <243362122@qq.com>
2019-03-18 16:04:24 +0800

    19-03-18

--- ktao/js/index.js
+++ ktao/js/index.js
@@ -1,8 +1,19 @@
 ;(function($){
-	$(".dropdown").dropdown({
+	var $menuDropdown = $(".dropdown");
+	$menuDropdown.dropdown({
 		delay:200,
 	});
-	$(".dropdown").on("dropdown-show",function(ev){
-		
+	$menuDropdown.on("dropdown-show",function(ev){
+		var $elem = $(this);
+		var loadUrl = $elem.data("load");
+		if(!loadUrl) return;
+		var layer = $elem.find(".dropdown-layer");
+		$.getJSON(loadUrl,function(data){
+			var html = "";
+			for(var i = 0;i<data.length;i++){
+				html += '<li><a href="'+data[i].url+'" class="menu-item">'+data[i].name+'</a></li>'
+			}
+			layer.html(html);
+		})
 	});
 })(jQuery);
