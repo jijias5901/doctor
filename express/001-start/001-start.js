6f0f4e8f341748e1e097fc5c3d3bb3cfa5ab51b4
Doctor <243362122@qq.com>
2019-03-29 19:20:20 +0800

    19-03-29 Undata name

--- express/001-start/001-start.js
+++ express/001-start/001-start.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use("/static",express,static("pubic"));
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.get("",(req,res)=>{
+
+})
 
 app.listen(3000, () => console.log('app listening on port 3000!'));
