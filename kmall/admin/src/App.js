b4cc445db70ad54b9ad125b40ae6c1ddcee94741
Doctor <243362122@qq.com>
2019-04-18 20:50:39 +0800

    19-04-18 updata kmall

--- kmall/admin/src/App.js
+++ kmall/admin/src/App.js
@@ -5,8 +5,11 @@
 * @Last Modified time: 2019-04-14 17:17:05
 */
 
-import React,{ Component,Fragment } from 'react'
-import Login from './pages/login'
+import React,{ Component,Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Login from './pages/login';
+import Home from './pages/Home';
 
 import './App.css'
 
@@ -14,10 +17,13 @@ import './App.css'
 class App extends Component{
 
 	render(){
-		return( 
-			<div className="App">
-				<Login />
-			</div>
+		return(
+			<Router>
+				<div className="App">
+					<Route exact path='/' component={ Home } />
+					<Route path='/login' component={ Login } />
+				</div>
+			</Router>
 		)
 	}
 }
--- kmall/admin/src/pages/login/index.js
+++ kmall/admin/src/pages/login/index.js
@@ -5,9 +5,12 @@
 * @Last Modified time: 2019-04-14 17:32:46
 */
 
-import React,{ Component } from 'react'
+import React,{ Component } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
+
 import {
-  Form, Icon, Input, Button, Checkbox,
+  Form, Icon, Input, Button, message,
 } from 'antd';
 
 import './index.css'
@@ -20,9 +23,28 @@ class NormalLoginForm extends Component {
   handleSubmit(e){
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
+		if (!err) {
+			this.setState(()=>({isFething:true}))
+			axios({
+				method:'post',
+				url:'http://127.0.0.1:3000/admin/login',
+				data:values
+			})
+			.then(result=>{
+				if(result.data.code == 0){//登录成功
+					//跳转后台首页
+					window.location.href = '/';
+				}else if(result.data.code == 1){
+					message.error(result.data.message);
+				}
+			})
+			.catch(err=>{
+				message.error('网络请求失败,请稍后再试');
+			})
+			.finally(()=>{
+				this.setState(()=>({isFething:false}))
+			})
+		}
     });
   }
 
@@ -32,21 +54,26 @@ class NormalLoginForm extends Component {
     	<div className="Login">
 			<Form className="login-form">
 			<Form.Item>
-			  {getFieldDecorator('username', {
-			    rules: [{ required: true, message: '请输入用户名!' },{ pattern: /^[a-z0-9_]$/i, message: '用户名为3-6位字符,字母数字下划线!'}],
-			  })(
-			    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" />
-			  )}
+				{getFieldDecorator('username', {
+					rules: [{ required: true, message: '请输入用户名!' },{ pattern: /^[a-z0-9_]{3,6}$/i, message: '用户名为3-6位字符,字母数字或下划线!'}],
+				})(
+					<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" />
+				)}
 			</Form.Item>
 			<Form.Item>
-			  {getFieldDecorator('password', {
-			    rules: [{ required: true, message: '请输入密码!' },{ pattern: /^\w$/i, message: '密码为3-6位字符'}],
-			  })(
-			    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" />
-			  )}
+				{getFieldDecorator('password', {
+					rules: [{ required: true, message: '请输入密码!' },{ pattern: /^\w{3,6}$/i, message: '密码为3-6位字符'}],
+				})(
+					<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" />
+				)}
 			</Form.Item>
 			<Form.Item>
-			  <Button type="primary" onClick={this.handleSubmit} className="login-form-button">
+			  	<Button 
+					type="primary" 
+					onClick={this.handleSubmit} 
+					className="login-form-button"
+					loading={this.props.isFething}
+				>
 			    登录
 			  </Button>
 			</Form.Item>
@@ -58,4 +85,16 @@ class NormalLoginForm extends Component {
 
 const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
 
-export default WrappedNormalLoginForm;
+const mapStateToProps = (state)=>{
+	return {
+		isFething:state.get('login').get('isFething')
+	}
+}
+
+const mapDispatchToProps = (dispatch)=>{
+	return {
+
+	}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(WrappedNormalLoginForm);
