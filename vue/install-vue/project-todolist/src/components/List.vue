0c2ebae8a37abfae94263fd13a864bb7c3dfdce2
Doctor <243362122@qq.com>
2019-05-13 20:55:26 +0800

    19-05-13 updata vue

---
+++ vue/install-vue/project-todolist/src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="List">
+		<Item v-for="ToDo in Todos" msg="ToDo" />
+	</div>
+</template>
+<script>
+	import Item from './Item.vue'
+	export default {
+		props:{
+			ToDos:Array
+		}
+		name:'List',
+		components:{
+			Item
+		}
+	}
+</script>
+<style scoped>
+	
+</style>
